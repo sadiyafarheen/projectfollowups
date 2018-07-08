30645e4da8eaa0addf2f551fb67239c9d7cb6222
Sadiya Farheen <farheensadiya26@gmail.com>
2018-07-08 20:37:49 +0530

    Add files via upload

---
+++ assets/AppAsset.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * @author Qiang Xue <qiang.xue@gmail.com>
+ * @since 2.0
+ */
+class AppAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/bootstrap.min.css',
+        'css/site.css',
+        'css/custom.css',
+        'css/font-awesome.css',
+        'css/font-awesome.min.css',
+    ];
+    public $js = [
+        'js/zabuto_calendar.min.js',
+        'js/custom.js',
+    ];
+    public $depends = [
+        'yii\web\YiiAsset',
+        'yii\bootstrap\BootstrapAsset',
+    ];
+}
