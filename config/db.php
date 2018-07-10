380d01e6b477883f6e2311ca2a91726efe846cb7
Hikmat Ullah <me@hikmatu.com>
2018-07-10 11:41:43 +0500

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=db;dbname=afitek_psb2',
+    'dsn' => 'mysql:host=db;dbname=pms_db',
     'username' => 'root',
     'password' => 'xsjr@343221##@@',
     'charset' => 'utf8',
